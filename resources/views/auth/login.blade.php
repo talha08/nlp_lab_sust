0779f377e8b26431075a2237bacba5af30621d58
ABU TALHA <talhaqc@gmail.com>
2016-11-05 01:56:40 +0600

    smrqej

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,149 +1,128 @@
-<!DOCTYPE Html>
-<Html lang="en">
+@extends('labfront.layouts.master')
+@section('content')
+    <div class="container">
+        <!-- container -->
 
-
-
-@include('includes.header')
-
-
-
-<body>
-
-<div class="wrapper-page animated fadeInDown">
-    <div class="panel panel-color panel-primary">
-        <div class="panel-heading">
-            <h3 class="text-center m-t-10"> <strong> Sign In</strong></h3>
+        <div class="row">
+            <div class="k-breadcrumbs col-lg-12 clearfix">
+                <!-- breadcrumbs -->
+                <ol class="breadcrumb">
+                    <li><a href="{!! route('labfront.index') !!}">SUST CSE NLP Lab</a></li>
+                    <li><a href="#">Login</a></li>
+                </ol>
+            </div>
+            <!-- breadcrumbs end -->
         </div>
+
+
+
+
+        <!-- ================== -->
         <br>
+        <!-- Vertical Steps Example -->
+        <div class="row">
+            <div class="col-sm-offset-3 col-md-6">
+                <div class="panel panel-default">
 
+                    <div class="panel-heading">
+                        <h3 class="text-center"><b>LogIn into NLP LAB, SUST</b></h3>
+                        @include('includes.alert')
+                    </div>
+                    <div class="panel-body">
+                        {!! Form::open(array('route' => 'login', 'method' => 'post', 'class' => 'form-horizontal m-t-40')) !!}
+                        <div class="form-group ">
+                            <div class="col-xs-12">
+                                {!! Form::text('email', '', array('class' => 'form-control','id' =>'text', 'placeholder' => 'Email Address', 'type'=>'text','autofocus')) !!}
+                            </div>
+                        </div>
+                        <div class="form-group ">
+                            <div class="col-xs-12">
+                                {!! Form::password('password', array('class' => 'form-control','id' =>'pass', 'placeholder' => 'Password','type'=>'text')) !!}
+                            </div>
+                        </div>
+                        <div class="form-group m-t-30">
+                            <div class="col-sm-5">
+                                <input checked="checked" name="agree" type="checkbox" value="1">Remember me
+                            </div>
+                            <div class="col-sm-7 text-right">
+                                <a data-toggle="modal" href="#myModal"><i class="fa fa-lock m-r-5"></i> Forgot your password?</a>
+                            </div>
+                        </div>
+                        {{--{!!--for csrf token--!!}--}}
+                        <div class="form-group text-right">
+                            <br>
+                            <div class="col-xs-12">
+                                {!! Form::submit('Log in', array('class' => 'btn btn-lg btn-login btn-block btn-purple ', 'type'=>'submit')) !!}
+                            </div>
+                        </div>
 
-             @include('includes.alert')
-
-
-
-            {!! Form::open(array('route' => 'login', 'method' => 'post', 'class' => 'form-horizontal m-t-40')) !!}
-            <div class="form-group ">
-                <div class="col-xs-12">
-                    {!! Form::text('email', '', array('class' => 'form-control', 'placeholder' => 'Email Address', 'type'=>'text','autofocus')) !!}
+                        <div class="registration">
+                            <center>
+                                <br>
+                                Don't have an account yet? <br/>
+                                <a class="" href="{!! route('user.create') !!}">
+                                    Apply For Account.
+                                </a>
+                                Or
+                                <a class="" href="{!! url('/') !!}">
+                                    Back To Homepage
+                                </a>
+                            </center>
+                        </div>
+                        {!! Form::close() !!}
+                    </div>
+                    <!-- End #wizard-vertical -->
                 </div>
+                <!-- End panel-body -->
             </div>
-
-            <div class="form-group ">
-
-                <div class="col-xs-12">
-                    {!! Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password','type'=>'text')) !!}
-                </div>
-            </div>
-
-
-
-        <div class="form-group m-t-30">
-
-                <div class="col-sm-5 ">
-                    <label class="cr-styled">
-                        <input checked="checked" name="agree" type="checkbox" value="1">
-                        <i class="fa"></i>
-                        Remember me
-                    </label>
-                </div>
-
-
-            <div class="col-sm-7 text-right">
-                <a data-toggle="modal" href="#myModal"><i class="fa fa-lock m-r-5"></i> Forgot your password?</a>
-            </div>
-
+            <!-- End panel -->
         </div>
-
-        {{--{!!--for csrf token--!!}--}}
-
-
-        <div class="form-group text-right">
-            <br>
-                 <div class="col-xs-12">
-                    {!! Form::submit('Log in', array('class' => 'btn btn-lg btn-login btn-block btn-purple ', 'type'=>'submit')) !!}
-                </div>
-        </div>
-
-
-        {{--{!!--<center>--!!}--}}
-        {{--{!!--<p>or you can sign in via social network</p>--!!}--}}
-
-        {{--{!!--<div class="login-social-link">--!!}--}}
-        {{--{!!--<a href="{!! route('login/fb') !!}" class="btn btn-primary"><i class="fa fa-facebook"></i> Facebook</a>--!!}--}}
-        {{--{!!--<!-- <a href="#" class="btn btn-info"><i class="fa fa-twitter"></i>Twitter</a> -->--!!}--}}
-        {{--{!!--<a href="{!! route('login/gp') !!}" class="btn btn-danger"><i class="fa fa-google-plus"></i> Google</a>--!!}--}}
-        {{--{!!--</div>--!!}--}}
-        {{--{!!--</center>--!!}--}}
-
-
-
-        <div class="registration">
-            <center>
-            <br>
-                Don't have an account yet? <br/>
-            <a class="" href="{!! route('user.create') !!}">
-                Apply For Account.
-            </a>
-                Or
-                <a class="" href="{!! url('/') !!}">
-                   Back To Homepage
-                </a>
-            </center>
-        </div>
-
-
-        {!! Form::close() !!}
-
+        <!-- end col -->
     </div>
-</div>
+    <!-- End row -->
 
 
 
 
-
-
-<!-- Modal -->
-<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Forgot Password ?</h4>
-            </div>
-            <div class="modal-body">
-                <p>Enter your e-mail address below to reset your password.</p>
-
-
-                {!! Form::open(array('action' => 'RemindersController@postEmail', 'method' => 'post')) !!}
-
-                <input type="hidden" name="_token" value="{!! csrf_token() !!}">
-                {!! Form::email('email', '', array('class' => 'form-control placeholder-no-fix', 'placeholder' => 'Email Address', 'autocomplete'=>'off')) !!}
-
-            </div>
-            <div class="modal-footer">
-                <button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>
-
-                {!! Form::submit('Submit', array('class' => 'btn btn-success')) !!}
-                {!! Form::close() !!}
+    <!-- Modal -->
+    <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Forgot Password ?</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Enter your e-mail address below to reset your password.</p>
+                    {!! Form::open(array('action' => 'RemindersController@postEmail', 'method' => 'post')) !!}
+                    <input type="hidden" name="_token" value="{!! csrf_token() !!}">
+                    {!! Form::email('email', '', array('class' => 'form-control placeholder-no-fix', 'placeholder' => 'Email Address', 'autocomplete'=>'off')) !!}
+                </div>
+                <div class="modal-footer">
+                    <button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>
+                    {!! Form::submit('Submit', array('class' => 'btn btn-success')) !!}
+                    {!! Form::close() !!}
+                </div>
             </div>
         </div>
     </div>
-</div>
-<!-- modal -->
+    <!-- modal -->
 
 
 
+@stop
+@section('style')
+            <!-- Bootstrap core CSS -->
+    {!! Html::style('css/bootstrap-reset.css') !!}
 
-
-</body>
-<!-- js placed at the end of the document so the pages load faster -->
-{!! Html::script('js/jquery.js') !!}
-{!! Html::script('js/bootstrap.min.js') !!}
-{!! Html::script('js/pace.min.js') !!}
-{!! Html::script('js/wow.min.js') !!}
-{!! Html::script('js/jquery.nicescroll.js') !!}
-
-        <!--common script for all pages-->
-{!! Html::script('js/jquery.app.js') !!}
-
-</Html>
+    <style>
+        #text{
+            font-size: medium;
+        }
+        #pass{
+            font-size: xx-large;
+        }
+    </style>
+@stop
+@section('script')
+@stop
