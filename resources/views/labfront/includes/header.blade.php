d9f2fbab1ee7d125f8e846a034176f2eef81af3e
ABU TALHA <talhaqc@gmail.com>
2017-01-27 00:23:34 +0600

    header title change

--- resources/views/labfront/includes/header.blade.php
+++ resources/views/labfront/includes/header.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Natural language Processing Lab,
+    <meta name="description" content="Natural language Processing Group,
     Computer Science and Engineering Department,
     Shahjalal University of Science and Technology, Sylhet.">
 	
