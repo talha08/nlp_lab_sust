f299f83803fcb97530153711c4dece3bf4bd83e6
Md. Abu Talha <talhaqc@gmail.com>
2017-05-15 16:06:19 +0600

    wer

--- public/nlpsust/corpus_api/veri_email.php
+++ public/nlpsust/corpus_api/veri_email.php
@@ -20,10 +20,10 @@ $name=$_COOKIE["name"];
 $token=$_COOKIE["token"];
 $email=$_COOKIE["email"];
 
-if(!class_exists('PHPMailer')) {
+//if(!class_exists('PHPMailer')) {
     require('phpmailer/class.phpmailer.php');
 	require('phpmailer/class.smtp.php');
-}
+//}
 
 require_once("mail_configuration.php");
 
