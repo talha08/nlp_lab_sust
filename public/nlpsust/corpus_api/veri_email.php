0da6e39c3842657bd8e3e14ca685accd04a6c42f
Md. Abu Talha <talhaqc@gmail.com>
2017-05-15 17:06:45 +0600

    Update veri_email.php

--- public/nlpsust/corpus_api/veri_email.php
+++ public/nlpsust/corpus_api/veri_email.php
@@ -20,10 +20,10 @@ $name=$_COOKIE["name"];
 $token=$_COOKIE["token"];
 $email=$_COOKIE["email"];
 
-if(!class_exists('PHPMailer')) {
+//if(!class_exists('PHPMailer')) {
     require('PHPMailer/class.phpmailer.php');
 	require('PHPMailer/class.smtp.php');
-}
+//}
 
 require_once("mail_configuration.php");
 
